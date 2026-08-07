455a33d152ffdb67f3e8d80249f95ead5ee0b8e4
marreuwjsj-collab <marreuwjsj@gmail.com>
2026-08-08 03:26:03 +0400

    feat: wire persistent message repository into DI

--- lib/core/di/providers.dart
+++ lib/core/di/providers.dart
@@ -1,13 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../features/chats/data/chat_repository.dart';
-import '../../features/messages/data/in_memory_message_repository.dart';
+import '../../features/messages/data/sqlite_message_repository.dart';
 import '../../features/messages/domain/message.dart';
+import '../storage/database.dart';
+import '../storage/secure_store.dart';
+
+final appDatabaseProvider = Provider<AppDatabase>((ref) {
+  final database = AppDatabase();
+  ref.onDispose(database.close);
+  return database;
+});
+
+final secureStoreProvider = Provider<SecureStore>((ref) {
+  return const FlutterSecureStore();
+});
 
 final chatRepositoryProvider = Provider<ChatRepository>((ref) {
   return DemoChatRepository();
 });
 
 final messageRepositoryProvider = Provider<MessageRepository>((ref) {
-  return InMemoryMessageRepository();
+  return SqliteMessageRepository(
+    database: ref.watch(appDatabaseProvider),
+    localUserId: 'local-user',
+  );
 });
